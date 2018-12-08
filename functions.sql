759121051661af6e668ce74d3bf2d4416900bd29
coolair 28 <you@emample.com>
2018-12-08 17:40:53 +0200

    All functions.

--- functions.sql
+++ functions.sql
@@ -2,20 +2,21 @@ CREATE OR REPLACE FUNCTION f_lopeta_laud(p_laua_kood laud.laua_kood%TYPE)
 RETURNS VOID AS $$
 UPDATE laud SET laua_seisundi_liik_kood=4
 WHERE laua_kood=p_laua_kood AND
-(laud.laua_seisundi_liik_kood = 2 OR
-laud.laua_seisundi_liik_kood = 3);
+(laua_seisundi_liik_kood = 2 OR
+laua_seisundi_liik_kood = 3);
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path=public, pg_temp;
 
+
 CREATE OR REPLACE FUNCTION f_registreeri_laud(
-p_laua_kood laud.laua_kood%TYPE,
-p_kohtade_arv laud.kohtade_arv%TYPE,
-p_reg_aeg laud.reg_aeg%TYPE,
-p_kommentaar laud.kommentaar%TYPE,
-p_laua_asukoht_kood laud.laua_asukoht_kood%TYPE,
-p_laua_materjali_kood laud.laua_materjali_kood%TYPE,
-p_isik_id laud.isik_id%TYPE)
-RETURNS laud.laua_kood%TYPE AS $$
+p_laua_kood laud.laua_kood integer,
+p_kohtade_arv laud.kohtade_arv integer,
+p_reg_aeg laud.reg_aeg timestamp,
+p_kommentaar laud.kommentaar text,
+p_laua_asukoht_kood laud.laua_asukoht_kood integer,
+p_laua_materjali_kood laud.laua_materjali_kood integer,
+p_isik_id laud.isik_id integer)
+RETURNS laud.laua_kood integer $$
 INSERT INTO laud
 (laua_kood, kohtade_arv, reg_aeg, kommentaar, laua_asukoht_kood, laua_materjali_kood, isik_id) VALUES
 (p_laua_kood, p_kohtade_arv, p_reg_aeg, p_kommentaar, p_laua_asukoht_kood, p_laua_materjali_kood, p_isik_id)
@@ -24,3 +25,43 @@ RETURNING laua_kood;
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path=public, pg_temp;
 
+
+CREATE OR REPLACE FUNCTION f_unusta_laud(p_laua_kood laud.laua_kood%TYPE)
+RETURNS VOID AS $$
+DELETE FROM laud 
+WHERE laua_kood=p_laua_kood AND
+laud.laua_seisundi_liik_kood = 1;
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
+
+
+CREATE OR REPLACE FUNCTION f_muuda_laua_andmeid(
+p_laua_kood_vana laud.laua_kood integer,
+p_laua_kood_uus laud.laua_kood integer,
+p_kohtade_arv laud.kohtade_arv integer,
+p_kommentaar TEXT,
+p_laua_materjali_kood laud.laua_materjali_kood integer,
+p_laua_asukoht_kood laud.laua_asukoht_kood integer
+)
+RETURNS VOID AS $$
+UPDATE laud SET laua_kood=p_laua_kood_uus, kohtade_arv=p_kohtade_arv, kommentaar=p_kommentaar, laua_asukoht_kood=p_laua_asukoht_kood, laua_materjali_kood=p_laua_materjali_kood
+WHERE laua_kood=p_laua_kood_vana
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
+
+
+CREATE OR REPLACE FUNCTION f_muuda_laud_aktiivseks(p_laua_kood laud.laua_kood%TYPE)
+RETURNS VOID AS $$
+UPDATE laud SET laua_seisundi_liik_kood=2
+WHERE laua_kood=p_laua_kood AND
+(laua_seisundi_liik_kood=1 OR laua_seisundi_liik_kood=3);
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
+
+CREATE OR REPLACE FUNCTION f_muuda_laud_mitteaktiivseks(p_laua_kood laud.laua_kood%TYPE)
+RETURNS VOID AS $$
+UPDATE laud SET laua_seisundi_liik_kood=3
+WHERE laua_kood=p_laua_kood AND
+laua_seisundi_liik_kood=2;
+$$ LANGUAGE SQL SECURITY DEFINER
+SET search_path=public, pg_temp;
