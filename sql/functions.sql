3149c013563d83f58bc83040689f482a50a7382e
Your Name <you@example.com>
2018-12-16 13:49:32 +0200

    int->smallint

--- sql/functions.sql
+++ sql/functions.sql
@@ -15,7 +15,7 @@ p_kommentaar laud.kommentaar % TYPE,
 p_laua_asukoht_kood laud.laua_asukoht_kood % TYPE,
 p_laua_materjali_kood laud.laua_materjali_kood % TYPE,
 p_isik_id laud.isik_id % TYPE)
-RETURNS integer AS $$
+RETURNS SMALLINT AS $$
 INSERT INTO laud
 (laua_kood, kohtade_arv, reg_aeg, kommentaar, laua_asukoht_kood, laua_materjali_kood, isik_id) VALUES
 (p_laua_kood, p_kohtade_arv, current_timestamp(0), p_kommentaar, p_laua_asukoht_kood, p_laua_materjali_kood, p_isik_id)
@@ -49,7 +49,7 @@ UPDATE laud SET laua_kood = p_laua_kood_uus, kohtade_arv = p_kohtade_arv, kommen
 WHERE laua_kood = p_laua_kood_vana
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path = public, pg_temp;
-COMMENT ON FUNCTION f_muuda_laud IS 'Funktsiooniga saab uuendab laua koodi, kohtade arvu, kommentaari, matejali koodi ja laua asukohta.';
+COMMENT ON FUNCTION f_muuda_laud IS 'Funktsiooniga uuendab andmebaasis laua koodi, kohtade arvu, kommentaari, matejali koodi ja laua asukohta.';
 
 
 CREATE OR REPLACE FUNCTION f_muuda_laud_aktiivseks(p_laua_kood laud.laua_kood % TYPE)
@@ -59,7 +59,7 @@ WHERE laua_kood = p_laua_kood AND
 (laua_seisundi_liik_kood = 1 OR laua_seisundi_liik_kood = 3);
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path = public, pg_temp;
-COMMENT ON FUNCTION f_muuda_laud_aktiivseks IS 'Funktsioon muudab laua mitteaktiivsest või ootel olekust aktiivsesse olekusse.';
+COMMENT ON FUNCTION f_muuda_laud_aktiivseks IS 'Funktsioon muudab andmebaasis laua mitteaktiivsest või ootel olekust aktiivsesse olekusse.';
 
 CREATE OR REPLACE FUNCTION f_muuda_laud_mitteaktiivseks(p_laua_kood laud.laua_kood % TYPE)
 RETURNS VOID AS $$
@@ -68,4 +68,4 @@ WHERE laua_kood = p_laua_kood AND
 laua_seisundi_liik_kood = 2;
 $$ LANGUAGE SQL SECURITY DEFINER
 SET search_path = public, pg_temp;
-COMMENT ON FUNCTION f_muuda_laud_mitteaktiivseks IS 'Funktsioon muudab laua aktiivsest olekust mitteaktiivsesse olekusse.';
+COMMENT ON FUNCTION f_muuda_laud_mitteaktiivseks IS 'Funktsioon muudab andmebaasis laua aktiivsest olekust mitteaktiivsesse olekusse.';
