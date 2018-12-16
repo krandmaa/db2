1d1b7b733d44c7fcb7a7be8f1505e976bc2a3105
Jordan <jordanjors@gmail.com>
2018-12-16 17:53:01 +0200

    Add domain to isik_elukoht

--- sql/t164844_schema.sql
+++ sql/t164844_schema.sql
@@ -198,7 +198,7 @@ reg_aeg d_reg_aeg,
 synni_kp date NOT NULL,
 eesnimi VARCHAR(1000) NULL,
 perenimi VARCHAR(1000) NULL,
-elukoht VARCHAR(255) NULL,
+elukoht d_nimetus NULL,
 CONSTRAINT pk_isik_isik_id PRIMARY KEY (isik_id),
 CONSTRAINT uc_isik_riik_kood_isikukood UNIQUE (riik_kood,isikukood),
 CONSTRAINT uc_isik_e_meil UNIQUE (e_meil),
@@ -210,8 +210,7 @@ isikukood ~* '^[a-z0-9 -\/]+$'),
 CONSTRAINT chk_isik_parool CHECK (trim(parool) <> ''),
 CONSTRAINT chk_isik_synni_kp_reg_aeg CHECK (synni_kp <= reg_aeg),
 CONSTRAINT chk_isik_synni_kp CHECK (synni_kp >= '1900-01-01 00:00:00'::timestamp without time zone AND synni_kp <= '2100-12-31 23:59:59'::timestamp without time zone),
-CONSTRAINT chk_isik_elukoht CHECK (trim(elukoht) <> '' AND
-NOT(REPLACE(elukoht, ' ', '') ~ '^[0-9\.]+$')),
+CONSTRAINT chk_isik_elukoht CHECK (NOT(REPLACE(elukoht, ' ', '') ~ '^[0-9\.]+$')),
 CONSTRAINT fk_isik_isiku_seisundi_liik_kood FOREIGN KEY (isiku_seisundi_liik_kood) REFERENCES isiku_seisundi_liik (isiku_seisundi_liik_kood) ON DELETE NO ACTION ON UPDATE CASCADE,
 CONSTRAINT fk_isik_riik_kood FOREIGN KEY (riik_kood) REFERENCES Riik (riik_kood) ON DELETE NO ACTION ON UPDATE CASCADE
 ) WITH (fillfactor=90)
